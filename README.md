f0fcfb80a1cb116bb3e02a15b905ecb7dc214b23
jmack847 <jmack847@gmail.com>
2018-03-04 16:29:47 -0600

    init new files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bash_scripts
 Collection of bash scripts I have collected over the years
+
+last updated 03/04/18
---
+++ ressh.bash
@@ -0,0 +1,9 @@
+#Directory and file permissions.
+
+
+chmod 700 ~/.
+
+chmod go-w ~/
+
+chmod 600 ~/.ssh/authorized_keys
+
